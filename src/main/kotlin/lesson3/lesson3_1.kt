a8571fa9c051a131eb18b1ee0508c629a4f5c297
Tereschin_SV <tereshin_ser@>
2026-07-22 11:11:07 +0300

    KS-3-1: string greeting

--- src/main/kotlin/lesson3/lesson3_1.kt
+++ src/main/kotlin/lesson3/lesson3_1.kt
@@ -1,2 +1,14 @@
 package org.example.lesson3
 
+fun main(){
+
+    val daytimeGreeting = "Доброе утро"
+    val eveningGreeting = "Добрый вечер"
+
+    val userName = "Пользователь"
+
+    var greetings = "$daytimeGreeting, $userName!"
+    println(greetings)
+    greetings = "$eveningGreeting, $userName!"
+    println(greetings)
+}
