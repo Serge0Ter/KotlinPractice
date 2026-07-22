9e8638471214d8bfed0f53fd245ead4e515f13c1
Tereschin_SV <tereshin_ser@>
2026-07-22 15:05:32 +0300

    KS-3-5: split — парсер сервера

--- src/main/kotlin/lesson3/lesson3_5.kt
+++ src/main/kotlin/lesson3/lesson3_5.kt
@@ -2,12 +2,12 @@ package org.example.lesson3
 
 fun main() {
 
-    var dataMove = "D2-D4;0"
+    val dataMove = "D2-D4;0"
 
     val items = dataMove.split(";")
-    var startMove = items[0].split("-")[0]
-    var endMove = items[0].split("-")[1]
-    var count = items[1]
+    val startMove = items[0].split("-")[0]
+    val endMove = items[0].split("-")[1]
+    val count = items[1]
 
     println("$startMove\n$endMove\n$count")
 }
