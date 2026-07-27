44b27774103960f7497c250c7ff8d5f32d412c84
Tereschin_SV <tereshin_ser@>
2026-07-27 12:18:13 +0300

    KS-4-3: Логика && и || — мониторинг растений

--- src/main/kotlin/lesson4/lesson4_3.kt
+++ src/main/kotlin/lesson4/lesson4_3.kt
@@ -1,16 +1,16 @@
 package org.example.lesson4
 
 const val AIR_HUMIDITY = 20
-const val SEASON = "зимма"
+const val SEASON = "зима"
 
 fun main() {
 
     val isSunny = true
     val isOpenTent = true
     val airHumidity = 20
-    val season = "зимма"
+    val season = "зима"
 
-    val result = ((isSunny == true) && (isOpenTent == true) && (airHumidity == AIR_HUMIDITY) && (season != SEASON))
+    val result = (isSunny && isOpenTent && (airHumidity == AIR_HUMIDITY) && (season != SEASON))
 
     println("Благоприятные ли условия сейчас для роста бобовых? $result")
 
