b3395c98c7c3c2ac2b4d71453794c7120a11a947
Tereschin_SV <tereshin_ser@>
2026-07-27 16:21:08 +0300

    KS-5-2: if-else — проверка совершеннолетия

--- src/main/kotlin/lesson5/lesson5_2.kt
+++ src/main/kotlin/lesson5/lesson5_2.kt
@@ -3,16 +3,12 @@ package org.example.lesson5
 import java.time.Year
 
 const val AGE_OF_MAJORITY = 18
-private val YEAR = Year.now().value
+private val currentYear = Year.now().value
 
 fun main() {
-
     print("Введите год своего рождения: ")
     val userYear = readln().toIntOrNull() ?: 0
-    val result = YEAR - userYear
-    when (result) {
-        in 0..17 -> println("Доступ запрещен")
-        in 18..100 -> println("Показать экран со скрытым контентом")
-        else -> println("Ошибка...")
-    }
+    val result = currentYear - userYear
+    if (result >= AGE_OF_MAJORITY) println("Показать экран со скрытым контентом")
+    else println("Доступ запрещен")
 }
