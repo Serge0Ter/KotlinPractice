b3cc64385e7f30cc8c1594c79ca94142f9d2a5c6
Tereschin_SV <tereshin_ser@>
2026-07-29 15:47:11 +0300

    KS-6-5: break — угадай число

---
+++ src/main/kotlin/lesson6/lesson6_5.kt
@@ -0,0 +1,30 @@
+package org.example.lesson6
+
+fun main() {
+
+    var attempt = 0
+
+    println("Что бы пройти авторизацию, докажите что вы не бот.")
+    while (attempt < 3) {
+        val number1 = (1..9).random()
+        val number2 = (1..9).random()
+        print(" Решите пример: $number1 + $number2 = ")
+        val sum = readln().toIntOrNull() ?: 0
+        when (sum) {
+            0 -> {
+                println("Некорректное значение")
+                attempt++
+            }
+
+            number1 + number2 -> {
+                println("Добро пожаловать")
+                return
+            }
+            else -> {
+                println("Ответ неверный")
+                attempt++
+            }
+        }
+    }
+    println("Доступ запрещен")
+}
