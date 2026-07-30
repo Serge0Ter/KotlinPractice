94b489899f245de8998c5b5fbe90a5a5da999296
Tereschin_SV <tereshin_ser@>
2026-07-30 15:26:37 +0300

    KS-7-2: downTo — авторизация по SMS

--- src/main/kotlin/lesson7/lesson7_2.kt
+++ src/main/kotlin/lesson7/lesson7_2.kt
@@ -3,14 +3,13 @@ package org.example.lesson7
 fun main() {
 
     while (true) {
-        val numbers = 1000..9999
-        val authCode = numbers.random()
+        val authCode = (1000..9999).random()
         println("Ваш код авторизации: $authCode")
         print("Введи код авторизации: ")
         val code = readln().toIntOrNull() ?: 0
         if (code == authCode) {
             println("Добро пожаловать")
             return
-        } else continue
+        }
     }
 }
