2a61035c33160545885620b217beae76b3a9dc40
Tereschin_SV <tereshin_ser@>
2026-07-31 09:56:32 +0300

    KS-8-1: arrayOf — статистика просмотров

--- src/main/kotlin/lesson8/lesson8_1.kt
+++ src/main/kotlin/lesson8/lesson8_1.kt
@@ -2,14 +2,7 @@ package org.example.lesson8
 
 fun main() {
 
-    val week = IntArray(7)
-
-    week.forEachIndexed { i, ch ->
-        print("Кол-во рекламных просмотров: ")
-        val count = readln().toIntOrNull() ?: -1
-        if (count > 0) week[i] = count
-    }
-
+    val week = intArrayOf(42, 31, 42, 38, 39, 48, 45)
     println("Всего рекламы просмотрено за неделю: ${week.sum()}")
 
 }
