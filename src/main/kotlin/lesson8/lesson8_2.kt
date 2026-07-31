7e61ddfa721fdb5a881082ff44c9a8f57ee1d9a9
Tereschin_SV <tereshin_ser@>
2026-07-31 10:24:04 +0300

    KS-8-2: Доступ к элементам — рецепт

---
+++ src/main/kotlin/lesson8/lesson8_2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson8
+
+fun main() {
+
+    val ingredients = arrayOf("морковь", "лук", "помидор", "мясо", "картошка", "перец")
+    print("Введи ингредиент для поиска: ")
+    val input = readln().trim().lowercase()
+    for (i in ingredients) {
+        if (i == input) {
+            println("Ингредиент $input в рецепте есть")
+            break
+        }
+        else println("Такого ингредиента в рецепте нет")
+    }
+}
