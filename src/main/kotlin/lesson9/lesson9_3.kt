2217d8c2167a02efc3b81de160caf955d41e0910
Tereschin_SV <tereshin_ser@>
2026-08-03 14:35:20 +0300

    KS-9-3: Операторы += — расчёт порций

--- src/main/kotlin/lesson9/lesson9_3.kt
+++ src/main/kotlin/lesson9/lesson9_3.kt
@@ -2,17 +2,13 @@ package org.example.lesson9
 
 fun main() {
 
-    val friedEgg = mapOf(
-        "яйца" to 2,
-        "молоко" to 50,
-        "масло" to 15
-    )
+    val friedEgg = listOf(2, 50, 15)
 
     print("Сколько порций хотите: ")
     val count = readln().toIntOrNull() ?: 0
     if (count != 0) println(
-        "На $count порций вам понадобится: яиц – ${friedEgg.getValue("яйца") * count}," +
-                " молока – ${friedEgg.getValue("молоко") * count}, сливочного масла – ${friedEgg.getValue("масло") * count}"
+        "На $count порций вам понадобится: яиц – ${friedEgg[0] * count}," +
+                " молока – ${friedEgg[1] * count}, сливочного масла – ${friedEgg[2] * count}"
     )
 
 }
