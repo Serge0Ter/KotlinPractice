76f5db645fa9101a823debefcc87790f08c5f35f
Tereschin_SV <tereshin_ser@>
2026-08-04 10:45:40 +0300

    KS-10-2: Параметры — валидация логина

---
+++ src/main/kotlin/lesson10/lesson10_2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson10
+
+fun main() {
+    print("Введите логин: ")
+    val login = readln().trim()
+    print("Введите пароль: ")
+    val password = readln().trim()
+    if (isValid(login, password)) println("Добро пожаловать") else println("Логин или пароль недостаточно длинные")
+}
+
+fun isValid(login: String, password: String): Boolean = login.length >= 4 && password.length >= 4
