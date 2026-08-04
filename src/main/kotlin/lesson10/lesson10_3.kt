f8a11f52130585a6389e9e74075cf4e2e034629c
Tereschin_SV <tereshin_ser@>
2026-08-04 11:58:54 +0300

    KS-10-3: Default параметры — генератор пароля

--- src/main/kotlin/lesson10/lesson10_3.kt
+++ src/main/kotlin/lesson10/lesson10_3.kt
@@ -2,16 +2,16 @@ package org.example.lesson10
 
 fun main() {
     print("Сколько символов должно быть в пароле: ")
-    println("Ваш пароль: ${generationPassword(readln().toIntOrNull() ?: 1)}")
+    println("Ваш пароль: ${generatePassword(readln().toIntOrNull() ?: 1)}")
 
 }
 
-fun generationPassword(length: Int): String {
+fun generatePassword(length: Int): String {
     val digital = 0..9
-    val symbol = listOf('!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ' ')
+    val symbol = ' '..'/'
     var password = ""
     for (i in 1..length) {
-        if (i % 2 == 0) password += digital.random() else password += symbol[(0..symbol.size - 1).random()]
+        if (i % 2 == 0) password += digital.random() else password += symbol.random()
     }
     return password
 }
