0466248aa08ad89b52c51c0bb71d138a14650953
Tereschin_SV <tereshin_ser@>
2026-08-10 11:57:34 +0300

    KS-12-4: require() — вывод в init

--- src/main/kotlin/lesson12/lesson12_4.kt
+++ src/main/kotlin/lesson12/lesson12_4.kt
@@ -1,23 +1,21 @@
 package org.example.lesson12
 
 
-class Weather4(day: Int, night: Int) {
+class Weather4(day: Int, night: Int, cloudy: Boolean) {
     private val celsius = 273
     val dayTemp: Int = day - celsius
     val nightTemp: Int = night - celsius
-    var isCloudy: Boolean = false
+    var isCloudy: Boolean = cloudy
 
     init {
-        require(day > 0) { "Температура $day ниже нуля" }
-        require(night > 0) { "Температура $night ниже нуля" }
+        require(day >= 0) { "Температура $day ниже нуля" }
+        require(night >= 0) { "Температура $night ниже нуля" }
         println("Днем: $dayTemp C Вечеров: $nightTemp C Дождь: $isCloudy")
     }
 }
 
 fun main() {
 
-    val friday = Weather4(100, 87)
-    val sunday = Weather4(105, 92).apply {
-        isCloudy = true
-    }
+    val friday = Weather4(100, 87, false)
+    val sunday = Weather4(105, 92, true)
 }
