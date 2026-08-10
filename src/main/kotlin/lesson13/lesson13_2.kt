bf4b2d4c988f6cd9eb5a9fce6a358caaa01f9413
Tereschin_SV <tereshin_ser@>
2026-08-10 14:34:33 +0300

    KS-13-2: Оператор ?. — безопасный вызов

--- src/main/kotlin/lesson13/lesson13_2.kt
+++ src/main/kotlin/lesson13/lesson13_2.kt
@@ -3,6 +3,14 @@ package org.example.lesson13
 class PhoneDirectory(val userName: String, val userPhone: Long, val userOrganization: String? = null) {
 
     fun printInfoUser() {
-        println("Имя: $userName\nНомер: $userPhone\nКомпания: ${userOrganization ?: "не указано"}")
+        println("Имя: $userName\nНомер: $userPhone\nКомпания: ${userOrganization ?: "<не указано>"}")
     }
+}
+
+
+fun main() {
+
+    val user = PhoneDirectory("Ser", 89123123)
+    user.printInfoUser()
+
 }
