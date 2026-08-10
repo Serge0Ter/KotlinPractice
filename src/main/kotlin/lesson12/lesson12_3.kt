6c5359423aa1ddbeb8ad2a18c858483eb04ad476
Tereschin_SV <tereshin_ser@>
2026-08-10 11:43:03 +0300

    KS-12-3: Secondary constructor — погода в Кельвинах

--- src/main/kotlin/lesson12/lesson12_3.kt
+++ src/main/kotlin/lesson12/lesson12_3.kt
@@ -1,21 +1,28 @@
 package org.example.lesson12
 
-class Weather3(
-    var dayTemp: Int,
-    var nightTemp: Int,
-    var isCloudy: Boolean,
-) {
+class Weather3(day: Int, night: Int) {
     private val celsius = 273
+    var dayTemp: Int = day - celsius
+    var nightTemp: Int = night - celsius
+    var isCloudy: Boolean = false
+
+    init {
+        require(day > 0)
+        require(night > 0)
+    }
+
     fun printInfo() {
-        println("Днем: ${dayTemp - celsius} Вечеров: ${nightTemp - celsius} Дождь: $isCloudy")
+        println("Днем: $dayTemp C Вечеров: $nightTemp C Дождь: $isCloudy")
     }
 
 }
 
 fun main() {
 
-    val friday = Weather3(100, 87, false)
-    val sunday = Weather3(105, 92, false)
+    val friday = Weather3(100, 87)
+    val sunday = Weather3(105, 92).apply {
+        isCloudy = true
+    }
     friday.printInfo()
     sunday.printInfo()
 }
