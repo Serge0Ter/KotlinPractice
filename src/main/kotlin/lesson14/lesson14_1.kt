703d3673a5c6f9e81ffe1e54ea232b46df6005ca
Tereschin_SV <tereshin_ser@>
2026-08-12 11:36:49 +0300

    KS-14-1: open class — корабли в игре

--- src/main/kotlin/lesson14/lesson14_1.kt
+++ src/main/kotlin/lesson14/lesson14_1.kt
@@ -1,15 +1,26 @@
 package org.example.lesson14
 
-open class Liner(open val speed: Int = 15, open val capacity: Int = 500)
-class Icebreaker(override val speed: Int, override val capacity: Int, val isBreakIce: Boolean = true) :
-    Liner(speed, capacity)
+open class Liner() {
+    open val speed: Int = 15
+    open val capacity: Int = 500
+}
 
-class Freighter(override val speed: Int, override val capacity: Int) : Liner(speed, capacity)
+class Icebreaker() : Liner() {
+    override val speed: Int = 10
+    override val capacity: Int = 300
+    val isBreakIce: Boolean = true
+}
+
+class Freighter() : Liner() {
+    override val speed: Int = 15
+    override val capacity: Int = 300
+    val cargo: Int = 600
+}
 
 fun main() {
 
     val liner = Liner()
-    val icebreaker = Icebreaker(10, 300)
-    val freighter = Freighter(15, 600)
+    val icebreaker = Icebreaker()
+    val freighter = Freighter()
 
 }
