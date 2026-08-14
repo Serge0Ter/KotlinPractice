fcaf3de520a9b745a2f91c9e7607a0adf85739b2
Tereschin_SV <tereshin_ser@>
2026-08-14 11:24:06 +0300

    KS-15-4: abstract class vs interface — музыкальный магазин

--- src/main/kotlin/lesson15/lesson15_4.kt
+++ src/main/kotlin/lesson15/lesson15_4.kt
@@ -1,8 +1,8 @@
 package org.example.lesson15
 
 abstract class Product(val name: String, val count: Int)
-abstract class Instrumental(name: String, count: Int) : Product(name, count), SearchComponent
-abstract class Components(name: String, count: Int) : Product(name, count)
+class Instrumental(name: String, count: Int) : Product(name, count), SearchComponent
+class Components(name: String, count: Int) : Product(name, count)
 
 interface SearchComponent {
     fun search(name: Components) {
