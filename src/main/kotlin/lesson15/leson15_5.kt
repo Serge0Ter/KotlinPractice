8c2e0d57603e33f7d73608937fe10f0f4a28e39d
Tereschin_SV <tereshin_ser@>
2026-08-17 14:55:12 +0300

    KS-15-5: default methods — грузовые и легковые автомобили

--- src/main/kotlin/lesson15/leson15_5.kt
+++ src/main/kotlin/lesson15/leson15_5.kt
@@ -1,63 +1,98 @@
 package org.example.lesson15
 
 class PassengerCar(override val maxPeople: Int = 3) : Drivable, BoardingLoading {
+
+    private var nowPeople: Int = 0
+
     override fun drive() {
         println("Еду быстро")
     }
 
     override fun boardingLoading(countPeople: Int) {
-        when (countPeople) {
-            0 -> println("Еду без пассажира")
-            1 -> println("Посадил 1 пассажира")
-            2 -> println("Посадил 2 пассажиров")
-            3 -> println("Посадил 3 пассажиров")
-            else -> {}
+        when ((nowPeople + countPeople) <= maxPeople) {
+            true -> {
+                nowPeople += countPeople
+                println("Посадил $nowPeople. Осталось ${maxPeople - nowPeople}")
+            }
+
+            false -> {
+                println("Нет мест")
+            }
         }
     }
 
     override fun unloadingPeople(countPeople: Int) {
-        when (countPeople) {
-            0 -> println("Ехал без пассажира")
-            1 -> println("Высаживаю 1 пассажира")
-            2 -> println("Высаживаю 2 пассажиров")
-            3 -> println("Высаживаю 3 пассажиров")
-            else -> {}
+        when ((nowPeople - countPeople) < 0) {
+            true -> {
+                println("Столько людей нет")
+            }
+
+            false -> {
+                println("Высаживаю $countPeople. Осталось ${nowPeople - countPeople}")
+                nowPeople -= countPeople
+            }
         }
     }
 }
 
-class Truck(override val maxPeople: Int = 1, override val maxCargo: Float) : Drivable, BoardingLoading, CargoLoading {
+class Truck(
+    override val maxPeople: Int = 1, override val maxCargo: Float = 2f,
+) : Drivable, BoardingLoading, CargoLoading {
+
+    private var nowPeopleTruck = 0
+    private var nowCargo = 0f
+
     override fun drive() {
         println("Еду не так быстро")
     }
 
     override fun boardingLoading(countPeople: Int) {
-        when (countPeople) {
-            0 -> println("Еду без пассажира")
-            1 -> println("Везу человека")
-            else -> {}
+        when ((nowPeopleTruck + countPeople) <= maxPeople) {
+            true -> {
+                nowPeopleTruck += countPeople
+                println("Посадил $nowPeopleTruck. Осталось ${maxPeople - nowPeopleTruck}")
+            }
+
+            false -> {
+                println("Нет мест")
+            }
         }
     }
 
     override fun unloadingPeople(countPeople: Int) {
-        when (countPeople) {
-            0 -> println("Ехал без пассажира")
-            1 -> println("Высаживаю 1 пассажира")
-            else -> {}
+        when ((nowPeopleTruck - countPeople) < 0) {
+            true -> {
+                println("Столько людей нет")
+            }
+
+            false -> {
+                println("Высаживаю $countPeople. Осталось ${nowPeopleTruck - countPeople}")
+                nowPeopleTruck -= countPeople
+            }
         }
     }
 
     override fun cargoLoading(countWeight: Float) {
-        when (countWeight) {
-            in 0f..2f -> println("Загрузили $countWeight т")
-            else -> {}
+        when ((nowCargo + countWeight) <= maxCargo) {
+            true -> {
+                nowCargo += countWeight
+                println("Загрузили $countWeight т. Осталось ${maxCargo - nowCargo}")
+            }
+
+            false -> println("Нет места")
         }
     }
 
     override fun unloadingCargo(countWeight: Float) {
-        when (countWeight) {
-            in 0f..2f -> println("Выгружаю $countWeight т")
-            else -> {}
+        when ((nowCargo - countWeight) < 0) {
+            true -> {
+                println("Столько груза нет")
+            }
+
+            false -> {
+                println("Выгружаю $countWeight т")
+                nowCargo -= countWeight
+            }
         }
     }
 
@@ -82,10 +117,17 @@ interface CargoLoading {
 
 fun main() {
 
-    val truck = Truck(0, 2f)
+    val truck = Truck(1, 2f)
     truck.cargoLoading(2f)
+    truck.boardingLoading(1)
+    truck.drive()
+    truck.unloadingCargo(1.5f)
     val car1 = PassengerCar(3)
     car1.boardingLoading(3)
+    car1.drive()
+    car1.unloadingPeople(3)
     val car2 = PassengerCar(3)
-    car2.boardingLoading(3)
+    car2.boardingLoading(2)
+    car2.drive()
+    car2.unloadingPeople(2)
 }
