e0d7756f6f88d840c50e525158909e200482cbe0
Tereschin_SV <tereshin_ser@>
2026-08-18 15:07:14 +0300

    KS-16-3: private set — круг с радиусом

--- src/main/kotlin/lesson16/lesson16_3.kt
+++ src/main/kotlin/lesson16/lesson16_3.kt
@@ -3,14 +3,13 @@ package org.example.lesson16
 class User(val login: String, initialPassword: String) {
     private val password = initialPassword
 
-    fun auth(password: String) {
-        if (password == this.password) println("Пароль введен корректно")
-        else println("Пароль некорректен")
+    fun auth(password: String): Boolean {
+        return password == this.password
     }
 }
 
 fun main() {
 
-    User("Ser", "321").auth("321")
+    println("Пароль введен верно? ${User(" Ser ", "321").auth("321")}")
 
 }
