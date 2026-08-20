5d00de7b88e4ee7f26519761333e07ca0897ccf6
Tereschin_SV <tereshin_ser@>
2026-08-20 16:27:37 +0300

    KS-18-3: smart cast — тамагочи с питомцами

---
+++ src/main/kotlin/lesson18/lesson18_3.kt
@@ -0,0 +1,31 @@
+package org.example.lesson18
+
+open class Pet(val name: String) {
+    open fun eat(): String {
+        return ""
+    }
+}
+
+class Cat(name: String) : Pet(name) {
+    override fun eat(): String {
+        return "ем рыбу"
+    }
+}
+
+class Fox(name: String) : Pet(name) {
+    override fun eat(): String {
+        return "ем ягоды"
+    }
+}
+
+class Dog(name: String) : Pet(name) {
+    override fun eat(): String {
+        return "ем кости"
+    }
+}
+
+fun main() {
+
+    val listPets = listOf<Pet>(Cat("Барсик"), Dog("Тузик"), Fox("Марфина"))
+    listPets.forEach { println("${it.name} -> ${it.eat()}") }
+}
