fb8561e08e6d7bc8e7b8282155916b56b7510f26
Tereschin_SV <tereshin_ser@>
2026-08-21 12:55:02 +0300

    KS-19-2: enum со свойствами — категории магазина

---
+++ src/main/kotlin/lesson19/lesson19_2.kt
@@ -0,0 +1,29 @@
+package org.example.lesson19
+
+enum class Categories(val title: String) {
+    CLOTHING("Одежда"), STATIONERY("Канцелярия"), OTHER("Остальное")
+}
+
+class Product(val title: String, val id: Int, val categories: Categories) {
+    fun printInfo() {
+        println(
+            when (categories) {
+                Categories.CLOTHING -> "Название товара: $title номер товара: $id категория товара: $categories"
+                Categories.STATIONERY -> "Название товара: $title номер товара: $id категория товара: $categories"
+                Categories.OTHER -> "Название товара: $title номер товара: $id категория товара: $categories"
+            }
+        )
+    }
+}
+
+fun main() {
+
+    val list = listOf(
+        Product("Футболка", 1, Categories.CLOTHING),
+        Product("Юбка", 2, Categories.CLOTHING),
+        Product("Пена", 3, Categories.OTHER),
+        Product("Скрепки", 4, Categories.STATIONERY),
+    )
+
+    list.forEach { it.printInfo() }
+}
